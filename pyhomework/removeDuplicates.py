be749da024bd1fd4b60cce36847d92734d8cc575
ishai-masada <75506144+ishai-masada@users.noreply.github.com>
2022-02-04 21:17:38 -0500

    Delete pyhomework directory

--- pyhomework/removeDuplicates.py
+++
@@ -1,8 +0,0 @@
-# Remove Duplicates from a List
-# Ishai Masada
-
-def removeDuplicates(someList):
-    someList = list(set(someList))
-    print(someList)
-
-removeDuplicates([1, 5, 5, 6, 6, 3, 3, 2, 5, 3, 1])
